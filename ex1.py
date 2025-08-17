9ecb0883eecb63a83c378e6db8010c02083495d5
karth <karhikdheeravath718@gmail.com>
2025-08-17 10:18:48 +0530

    added

--- ex1.py
+++ ex1.py
@@ -2,3 +2,5 @@ print("Today Working On The Cyclone App")
 print("Cyclone Branch")
 print("Always Work in the Cyclone Branch only")
 print("I Want To work on the merge here")
+
+print("Cyclone as as app")
